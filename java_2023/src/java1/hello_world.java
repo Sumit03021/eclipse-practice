79dea50c543bcc8c6559cf9a31185fc62a0cc79d
Sumit03021 <sumitkumar03021997@gmail.com>
2023-09-28 01:36:25 -0700

    first

---
+++ java_2023/src/java1/hello_world.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hello world");
+ System.out.println("hello my world");
+	}
+
+}
